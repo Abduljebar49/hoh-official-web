91f9318bd2ccc03386e83f7b85a27ce786769fc7
Abduljebar <57292618+Abduljebar49@users.noreply.github.com>
2024-02-08 12:57:18 +0300

    new hero bg added

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -3,10 +3,13 @@ import Img2 from './2.jpg';
 import Img3 from './3.jpg';
 import Menu from './menu.png';
 import Moto from './moto.jpg'
+import HeroBg from './hero_bg.jpg'
+
 export {
     Img1,
     Img2,
     Img3,
     Menu,
-    Moto
+    Moto,
+    HeroBg
 }
